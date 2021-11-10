83f4fcfed70bd7cb52c85c757a89ad2970398fde
Mitul123 <sharmamitul72@gmail.com>
2021-11-10 11:09:16 +0530

    ? added to avoid sql injection

--- todo-api/model/create.go
+++ todo-api/model/create.go
@@ -2,8 +2,19 @@ package model
 
 import "fmt"
 
-func CreateTodo() error {
+func CreateTodo(name, todo string) error {
 	insertQ, err := con.Query("INSERT INTO TODO VALUES(?,?)", "Mitul", "Coding")
+	// to avoid sql injection added ?
+	defer insertQ.Close()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func DeleteTodo(name string) error {
+	insertQ, err := con.Query("DELETE FROM TODO WHERE name=?)", name)
 	defer insertQ.Close()
 	if err != nil {
 		fmt.Println(err)
