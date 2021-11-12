77f337dafeb4d431e81d2e484c6bc5788bf9aeb0
Mitul123 <sharmamitul72@gmail.com>
2021-11-12 18:17:29 +0530

    stack traces added

--- recover/main.go
+++ recover/main.go
@@ -4,14 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime/debug"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/panic", panicDemo)
-	mux.HandleFunc("/panic-after", panicAfterDemo)
+	mux.HandleFunc("/panic/", panicDemo)
+	mux.HandleFunc("/panic-after/", panicAfterDemo)
 	//mux.HandleFunc("/", hello)
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	log.Fatal(http.ListenAndServe(":3000", recoverMw(mux)))
+}
+
+func recoverMw(app http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println(err)
+				stack := debug.Stack()
+				log.Println(string(stack))
+				http.Error(w, "something went wrong", http.StatusInternalServerError)
+			}
+		}()
+
+		app.ServeHTTP(w, r)
+	}
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
@@ -28,5 +44,11 @@ func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
 }
 
 func funcThatPanics() {
-	panic("oh NO !")
+	panic("oh NO!")
 }
+
+/*
+func hello() {
+	fmt.Println("Hello")
+}
+*/
