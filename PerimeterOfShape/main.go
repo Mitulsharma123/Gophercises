783597b47205ee013b04e04dc81c820bd07eb918
Mitul123 <sharmamitul72@gmail.com>
2022-01-17 12:54:49 +0530

    comment and statement added

--- PerimeterOfShape/main.go
+++ PerimeterOfShape/main.go
@@ -1,33 +1,25 @@
+//Add a new method to the Shape interface called perimeter which calculates the perimeter of a shape. Implement the method for Circle and Rectangle.
+
 package main
 
 import ("fmt"; "math")
 
-type Shape interface{
+type Shape interface{  //interface Shape 
   area() float64
   perimeter() float64
 }
 
-func distance(x1, y1, x2, y2 float64) float64 {
+func distance(x1, y1, x2, y2 float64) float64 {  //function distance 
   a := x2 - x1
   b := y2 - y1
   return math.Sqrt(a*a + b*b)
 }
 
-/*func rectangleArea(x1, y1, x2, y2 float64) float64 {
-  l := distance(x1, y1, x1,y2)
-  w := distance(x1, y1, x2, y1)
-  return l * w
+type Circle struct{  //defining a struct type of circle
+  x, y, r float64    //list of field
 }
 
-func circleArea(x, y, r float64) float64 {
-  return math.Pi * r*r
-}
-*/
-type Circle struct{
-  x, y, r float64
-}
-
-type Rectangle struct {
+type Rectangle struct { ////defining a struct type of rectangle
   x1,y1,x2,y2 float64
 } 
 
@@ -36,18 +28,15 @@ func (r *Rectangle) area() float64 {
   w := distance(r.x1, r.y1, r.x2, r.y1)
   return l * w
 }
+//In between the keyword func and the name of the function we've added a “receiver”. 
+//The receiver is like a parameter – it has a name and a type –
+//but by creating the function in this way it allows us to call the function using the . operator
 
 func (c *Circle) area() float64{
   return math.Pi * c.r * c.r
 }
 
 func main() {
-  //var rx1, ry1 float64 = 0, 0
-  //var rx2, ry2 float64 = 10, 10
-  //var cx, cy, cr float64 = 0, 0, 5
-  
-  //fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
-  //fmt.Println(circleArea(cx, cy, cr))
   
   c := Circle{1,1,5}
   r := Rectangle{1, 1, 10, 10}
@@ -56,6 +45,8 @@ func main() {
   fmt.Println(r.area())
 
 }
+
+//perimeter method to calculate perimeter of shape
 
 func (r *Rectangle) perimeter() float64 {
   l := distance(r.x1, r.y1, r.x1, r.y2)
